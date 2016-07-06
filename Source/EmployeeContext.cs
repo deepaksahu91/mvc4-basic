dd98b3fd1c32544bc8afe1e34cfcc8065bdd74da
deepaksahu91 <deepaksahuorissa@gmail.com>
2016-07-06 18:30:12 +0530

    Update EmployeeContext.cs

--- Source/EmployeeContext.cs
+++ Source/EmployeeContext.cs
@@ -1,1 +1,13 @@
-Employee
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace mvc7.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
